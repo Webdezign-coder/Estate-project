deeff74e93e953b0c1c95bc387c3d67e3ccb38d1
web coder <afshan823001@gmail.com>
2025-05-25 21:29:51 +0530

    Add files

---
+++ src/assets/assets.js
@@ -0,0 +1,85 @@
+import houseimage1 from "./houseimage1.webp";
+import houseimage2 from "./houseimage2.png";
+import houseimage3 from "./houseimage3.jpeg";
+import houseimage4 from "./houseimage4.jpeg";
+import houseimage5 from "./houseimage5.jpeg";
+import houseimage6 from "./houseimage6.jpeg";
+import houseimg from "./houseimg.jpg";
+import person from "./person.jpg";
+import person2 from "./person2.jpg";
+import person3 from "./person3.jpg";
+import menuicon from "./menuicon.png";
+import close from "./close.png";
+import rightarrow from "./rightarrow.png";
+import leftarrow from "./leftarrow.png";
+export const assets = {
+  houseimage1,
+  houseimage2,
+  houseimage3,
+  houseimage4,
+  houseimage5,
+  houseimage6,
+  houseimg,
+  menuicon,
+  person,
+  person2,
+  person3,
+  close,
+  rightarrow,
+  leftarrow,
+};
+export const projectsData = [
+  {
+    title: "Willow Crest",
+    price: "$1,200,000",
+    location: "California",
+    image: "src/assets/houseimage1.webp",
+  },
+  {
+    title: "Mountain Retreat",
+    price: "$850,000",
+    location: "Colorado",
+    image: "src/assets/houseimage2.png",
+  },
+  {
+    title: "Urban Oasis",
+    price: "$1,500,000",
+    location: "New York",
+    image: "src/assets/houseimage3.jpeg",
+  },
+  {
+    title: "Lakeside Escape",
+    price: "$950,000",
+    location: "Minnesota",
+    image: "src/assets/houseimage4.jpeg",
+  },
+  {
+    title: "Modern Marvel",
+    price: "$2,000,000",
+    location: "California",
+    image: "src/assets/houseimage5.jpeg",
+  },
+  {
+    title: "Countryside Charm",
+    price: "$750,000",
+    location: "Texas",
+    image: "src/assets/houseimage6.jpeg",
+  },
+];
+export const testimonialsData = [
+  {
+    name: "John Doe",
+    image: "src/assets/person.jpg",
+    text: '"This is the best real estate website I have ever used. Highly recommend!"',
+  },
+  {
+    name: "Jane Smith",
+    image: "src/assets/person2.jpg",
+    text: '"The customer service was exceptional and they helped me find my dream home."',
+  },
+  {
+    name: "Michael Johnson",
+    image: "src/assets/person3.jpg",
+    text: '"I was impressed with the variety of properties available. Great experience!"',
+  },
+];
